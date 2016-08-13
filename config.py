5d532921747893111903bb25a3335f0d7ff83d13
whoami <19941222hb@gmail.com>
2016-08-13 22:40:29 +0800

    add config file for mlnotifer

---
+++ config.py
@@ -0,0 +1,23 @@
+# coding:utf-8
+
+config = {
+    "groups":[
+         {
+             "host":"imap.163.com",
+             "user":"18281573692@163.com",
+             "password":"huangbiao526114",
+             "email_wlist":["*"],
+             "email_blist":[""],
+             "notify_phone":["18281573692",]
+         },
+         {
+             "host":"imap.gmail.com",
+             "user":"19941222hb@gmail.com",
+             "password":"xxxxxxxxx",
+             "email_wlist":["*"],
+             "email_blist":[""],
+             "notify_phone":["18281573692"],
+         },   
+     ]
+}
+
