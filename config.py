fce287d74718bd33a1b09960277da604dd92d8a7
whoami <19941222hb@gmail.com>
2016-08-13 23:20:57 +0800

    add 'port'  config

--- config.py
+++ config.py
@@ -5,7 +5,8 @@ config = {
          {
              "host":"imap.163.com",
              "user":"18281573692@163.com",
-             "password":"huangbiao526114",
+             "password":"huangbiao526114", 
+             "port":"",
              "email_wlist":["*"],
              "email_blist":[""],
              "notify_phone":["18281573692",]
@@ -13,7 +14,8 @@ config = {
          {
              "host":"imap.gmail.com",
              "user":"19941222hb@gmail.com",
-             "password":"xxxxxxxxx",
+             "password":"huangbiao526114",
+             "port":993,
              "email_wlist":["*"],
              "email_blist":[""],
              "notify_phone":["18281573692"],
