e8f99efead26f16ba5f78ae2ed3dc3b9b054d16e
jell <infinite.ft@gmail.com>
2017-02-07 17:08:02 +0800

    delete unused code

--- mlnotifer.py
+++ mlnotifer.py
@@ -31,9 +31,9 @@ class ImapReceiver(imaplib.IMAP4_SSL):
         typ, data = self.recent()
         return int(data[0].decode())
     
-    def get_recent_mail_header(self, mailbox="INBOX", readonly=False):
+    def get_recent_mail_header(self, mailbox="INBOX", readonly=True):
         self.select(mailbox, readonly)
-        typ, data = self.search(None, "RECENT")
+        typ, data = self.search(None, "NEW")
         msg_set = data[0].split()[::-1]
         msg_headers = []
         for msg_id in msg_set:
@@ -44,38 +44,3 @@ class ImapReceiver(imaplib.IMAP4_SSL):
                 print("get mail %s error" % msg_id)
         return msg_headers
     
-    
-#recv = ImapReceiver(HOST, USERNAME, PASSWORD)
-#recv.active_receiver()
-#print(recv.get_total_mail_num())
-#print(recv.get_recent_mail_num())
-#print(recv.get_recent_mail_header(readonly=True))
-#exit()
-if __name__ == '__main__':
-    HOST = "imap.gmail.com"
-    USER = "19941222hb@gmail.com"
-    PASSWORD = "huangbiao526114"
-    imap_server = imaplib.IMAP4_SSL(HOST)
-    imap_server.login(USER, PASSWORD)
-    msgnums = imap_server.select("INBOX",readonly=True)
-    print(imap_server.list())
-    print(imap_server.recent())
-    #exit()
-    typ, data = imap_server.search(None, '(SEEN)')
-    # reverse msg id 
-    #print(data)
-    data = data[0].split()[::-1]
-    print(len(data))
-    #print(data)
-    exit()
-    msg = []
-    for msgid in data:
-        try:
-            typ, data = imap_server.fetch(msgid, '(BODY[HEADER])') 
-            header_dict = dict(email.message_from_string(data[0][1].decode()))
-            msg.append(header_dict)
-            print(header_dict)
-        except Exception as e:
-            print(e)
-            print("get mail %s failed!" % msgid)
-    print(msg)
