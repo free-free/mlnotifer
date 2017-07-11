659a3857ea4b7efc42284544c29bf92b8a41b993
jell <infinite.ft@gmail.com>
2017-07-11 17:34:09 +0800

    implement get_notifers() and get_notifer_by_sv() help function

--- mlnotifer/notifer.py
+++ mlnotifer/notifer.py
@@ -17,6 +17,8 @@ class SMSNotifer(object):
 
 class TwilioSMSNotifer(SMSNotifer):
 
+    SV = 'twilio'
+
 
     def __init__(self, acount_id=None, token=None):
         super(SMSDriver, self).__init__()
@@ -29,7 +31,39 @@ class TwilioSMSNotifer(SMSNotifer):
 
 class SubmailSMSNotifer(SMSNotifer):
     
-    
+    SV = 'submail'
+
+
     def notify(self, from_, to_, body):
         pass
+
+
+def get_notifers():
+    """
+    get all notifer class
+    return the list of notifer class
+    """
+    def get_subclasses(cls):
+        subclasses = set()
+        for subclass in cls.__subclasses__():
+            subclasses.add(subclass)
+            subclasses.update(get_subclasses(subclass))
+        return subclasses
+    return [notifer for notifer in list(get_subclasses(SMSNotifer))\
+            if hasattr(notifer, 'SV') and notifer.SV]
+
+
+def get_notifer_by_sv(sv=None):
+    """
+    returns: notifer class
+    """
+    if not sv or type(sv) is not str:
+        raise TypeError("Invalid sv %s" % sv)
+    selected_notifers = list(filter(lambda notifer: hasattr(notifer, 'SV') \
+        and notifer.SV == sv, get_notifers()))
+    if len(selected_notifers) == 0:
+        raise ValueError("Can't found notifer for sv '%s'" % sv)
+    else:
+        return selected_notifers[0]
+
 
